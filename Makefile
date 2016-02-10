bb6d5ff0123b169c97bcf06627a73899e1c77f7f
Paul Brook <paul@nowt.org>
2016-02-10 16:33:23 +0000

    Linear code
    Signed-off-by: Paul Brook <paul@nowt.org>

--- Makefile
+++ Makefile
@@ -10,6 +10,8 @@ TT = TARGET_K64F/TOOLCHAIN_GCC_ARM
 MBED = mbed/build/mbed
 MBED_LIBDIR = $(MBED)/$(TT)
 USB = mbed/build/usb
+RTOS = mbed/build/rtos
+ETH = mbed/build/net/eth
 
 SYS_OBJECTS = $(MBED_LIBDIR)/mbed_overrides.o $(MBED_LIBDIR)/system_MK64F12.o $(MBED_LIBDIR)/startup_MK64F12.o $(MBED_LIBDIR)/board.o $(MBED_LIBDIR)/cmsis_nvic.o $(MBED_LIBDIR)/retarget.o 
 
@@ -25,9 +27,23 @@ MBED_INCLUDES = -I$(MBED) -I$(MBED)/TARGET_K64F \
 	-I$(MBED_TARGET_DIR)/TARGET_MCU_K64F/device/MK64F12 \
 	-I$(MBED_TARGET_DIR)/TARGET_KPSDK_CODE/hal/gpio
 
-INCLUDE_PATHS = $(MBED_INCLUDES) $(USB_INCLUDES)
-LIBRARY_PATHS = -L$(MBED_LIBDIR) -L$(USB)/$(TT)
-LIBRARIES = -lmbed
+RTOS_INCLUDES = \
+	-I$(RTOS) \
+	-I$(RTOS)/TARGET_CORTEX_M
+
+ETH_INCLUDES = \
+	-I$(ETH)/EthernetInterface \
+	-I$(ETH)/lwip/include \
+	-I$(ETH)/lwip/include/ipv4 \
+	-I$(ETH)/lwip \
+	-I$(ETH)/lwip-sys \
+	-I$(ETH)/lwip-eth/arch/TARGET_Freescale \
+	-I$(ETH) \
+	-I$(ETH)/Socket
+
+INCLUDE_PATHS = $(MBED_INCLUDES) $(USB_INCLUDES) $(RTOS_INCLUDES) $(ETH_INCLUDES)
+LIBRARY_PATHS = -L$(MBED_LIBDIR) -L$(USB)/$(TT) -L$(RTOS)/$(TT) -L$(ETH)/$(TT)
+LIBRARIES = -leth -lrtos -lrtx -lmbed
 LINKER_SCRIPT = $(MBED_LIBDIR)/K64FN1M0xxx12.ld
 
 ############################################################################### 
@@ -47,7 +63,7 @@ endif
 
 
 CPU = -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=$(FLOAT_ABI) 
-CC_FLAGS = $(CPU) -c -g -fno-common -fmessage-length=0 -Wall -fno-exceptions -ffunction-sections -fdata-sections -fomit-frame-pointer -MMD -MP
+CC_FLAGS = $(CPU) -c -g -fno-common -fmessage-length=0 -Wall -fno-exceptions -ffunction-sections -fdata-sections -fomit-frame-pointer -MMD -MP -fshort-wchar
 CC_SYMBOLS = -DTARGET_KPSDK_MCUS -DTARGET_FF_ARDUINO -DTOOLCHAIN_GCC_ARM -DTOOLCHAIN_GCC -DCPU_MK64FN1M0VMD12 -DTARGET_FRDM -DTARGET_CORTEX_M -D__FPU_PRESENT=1 -DTARGET_KPSDK_CODE -DTARGET_M4 -D__MBED__=1 -DTARGET_K64F -DTARGET_Freescale -D__CORTEX_M4 -DFSL_RTOS_MBED -DTARGET_MCU_K64F -DARM_MATH_CM4 
 
 LD_FLAGS = $(CPU) -Wl,--gc-sections --specs=nano.specs -u _printf_float -u _scanf_float -Wl,--wrap,main -Wl,-Map=$(PROJECT).map,--cref
--- main.cpp
+++ main.cpp
@@ -1,29 +1,36 @@
 #include "mbed.h"
+#include "EthernetInterface.h"
 
 #define COLUMNS 32
 #define SPEED 1
-#define BASE 10
+#define BASE 8
+
+Serial pc(USBTX, USBRX);
 
 DigitalOut myled(LED_RED);
+DigitalOut green_led(LED_GREEN);
 
+#define PANEL_MEM_BASE 0xa0000000
+#define PANEL_MEM (*(volatile uint8_t *)PANEL_MEM_BASE)
+
+#if 0
 DigitalOut r1(PTD6);
 DigitalOut g1(PTD5);
 DigitalOut b1(PTD3);
-//DigitalOut gnd1(PTC2);
 DigitalOut r2(PTD2);
 DigitalOut g2(PTC9);
 DigitalOut b2(PTC8);
-//DigitalOut gnd2(PTB23);
+DigitalOut clk(PTD1);
+#endif
 
 DigitalOut sel_a(PTC0);
 DigitalOut sel_b(PTC7);
 DigitalOut sel_c(PTC5);
-//DigitalOut gnd3(PTB9);
 
-DigitalOut clk(PTD1);
 DigitalOut lat(PTB19);
 DigitalOut oe(PTC1);
-//DigitalOut gnd4(PTC6);
+
+Timer t;
 
 static void
 select_row(int n)
@@ -33,45 +40,176 @@ select_row(int n)
   sel_c = (n >> 2) & 1;
 }
 
+static void
+setup_flexbus(void)
+{
+  SIM_CLKDIV1 = (SIM_CLKDIV1 &~ SIM_CLKDIV1_OUTDIV3_MASK) | SIM_CLKDIV1_OUTDIV3(2);
+  SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK | SIM_SCGC5_PORTD_MASK;
+  SIM_SCGC7 |= SIM_SCGC7_FLEXBUS_MASK;
+  FB_CSMR0 = 0;
+  FB_CSAR0 = PANEL_MEM_BASE;
+  FB_CSCR0 = FB_CSCR_BLS_MASK | FB_CSCR_AA_MASK | FB_CSCR_PS(1);
+  FB_CSMR0 = FB_CSMR_V_MASK;
+  PORTD_PCR6 = PORT_PCR_MUX(5) | PORT_PCR_DSE_MASK;
+  PORTD_PCR5 = PORT_PCR_MUX(5) | PORT_PCR_DSE_MASK;
+  PORTD_PCR3 = PORT_PCR_MUX(5) | PORT_PCR_DSE_MASK;
+  PORTD_PCR2 = PORT_PCR_MUX(5) | PORT_PCR_DSE_MASK;
+  PORTC_PCR9 = PORT_PCR_MUX(5) | PORT_PCR_DSE_MASK;
+  PORTC_PCR8 = PORT_PCR_MUX(5) | PORT_PCR_DSE_MASK;
+  PORTD_PCR1 = PORT_PCR_MUX(5) | PORT_PCR_DSE_MASK;
+  myled = 1;
+}
+
+static void
+setup_dma(void)
+{
+  SIM_SCGC6 |= SIM_SCGC6_DMAMUX_MASK;
+  SIM_SCGC7 |= SIM_SCGC7_DMA_MASK;
+  DMA_TCD0_SADDR = 0;
+  DMA_TCD0_SOFF = 4;
+  DMA_TCD0_ATTR = DMA_ATTR_SSIZE(2) | DMA_ATTR_DSIZE(0);
+  DMA_TCD0_NBYTES_MLNO = COLUMNS;
+  DMA_TCD0_SLAST = 0;
+  DMA_TCD0_DADDR = PANEL_MEM_BASE;
+  DMA_TCD0_DOFF = 0;
+  DMA_TCD0_CITER_ELINKNO = 1;
+  DMA_TCD0_DLASTSGA = 0;
+  DMA_TCD0_CSR = 0; //DMA_CSR_INTMAJOR_MASK;
+  DMA_TCD0_BITER_ELINKNO = 1;
+}
+
+uint8_t framebuffer[COLUMNS * 8];
+
+static void
+trigger_dma(uint32_t src)
+{
+  DMA_TCD0_SADDR = src;
+  DMA_SSRT = 0;
+}
+
+EthernetInterface eth;
+
+#define PACKET_SIZE (COLUMNS * 8)
+uint8_t packet_buffer[PACKET_SIZE];
+
+static void
+ethernet_thread(const void *arg)
+{
+  UDPSocket s;
+  Endpoint client;
+  int n;
+
+  eth.init();
+  eth.connect();
+  s.bind(3001);
+
+  while(1) {
+      n = s.receiveFrom(client, (char *)packet_buffer, sizeof(packet_buffer));
+      if (n == PACKET_SIZE) {
+	  memcpy(framebuffer, packet_buffer, n);
+      }
+  }
+}
+
+#if 0
+static void scan_tick(void);
+Timeout scan_timeout;
+
+static bool output_active;
+static bool dma_active;
+static const uint8_t *read_framebuffer;
+
+#define BASE_SCAN_US 4
+
+static void
+maybe_rearm(void)
+{
+  static int current_row;
+  static int current_subframe;
+  static const uint8_t *ptr;
+
+  if (output_active || dma_active)
+    return;
+
+  output_active = true;
+  lat = 1;
+  if (current_subframe == 0) {
+      select_row(current_row);
+  }
+  scan_timeout.attach(scan_tick, BASE_SCAN_US << current_subframe);
+  lat = 0;
+  if (ptr) {
+      oe = 0;
+  }
+  current_subframe++;
+  if (current_subframe == NUM_SUBFRAMES) {
+      current_subframe = 0;
+      current_row++;
+      if (current_row == 8) {
+	  current_row = 0;
+	  ptr = read_framebuffer;
+      }
+  }
+  if (ptr) {
+      dma_active = true;
+      trigger_dma(ptr);
+      ptr += COLUMNS;
+  }
+}
+
+static void
+scan_tick(void)
+{
+  oe = 1;
+  output_active = false;
+  maybe_rearm();
+}
+#endif
+
 int main()
 {
+  Thread thread(ethernet_thread);
   int i;
   int row;
   int x;
   int match1;
   int match2;
-  int delay;
+  uint8_t *p;
+  int next_tick;
+  int now;
 
-  //gnd1 = 0;
-  myled = 0;
+  green_led = 1;
+  setup_flexbus();
+  setup_dma();
   oe = 1;
-  clk = 0;
   lat = 0;
-  r1 = 0;
-  r2 = 0;
-  g1 = 0;
-  g2 = 0;
-  b1 = 0;
-  b2 = 0;
+  x = 0;
+  myled = 1;
 
   row = 0;
-  select_row(row);
+  t.start();
+  next_tick = 0;
   while(1) {
-      r1 = 0;
+      p = &framebuffer[COLUMNS * row];
       match1 = (x / ((row + BASE) * SPEED)) % COLUMNS;
       match2 = (x / ((row + BASE + 8) * SPEED)) % COLUMNS;
       for (i = 0; i < COLUMNS; i++) {
-	  r1 = (i == match1);
-	  r2 = (i == match2);
-	  g1 = (i == match1);
-	  g2 = (i == match2);
-	  b1 = (i == match1);
-	  b2 = (i == match2);
-	  clk = 1;
-	  r1 = 0;
-	  r2 = 0;
-	  clk = 0;
+	  uint8_t val;
+	  val = 0;
+	  if (i == match1)
+	    val |= 0x0a;
+	  if (i == match2)
+	    val |= 0xc0;
+	  p[i] = val;
       }
+      myled = 1;
+      trigger_dma((uint32_t)p);
+      now = t.read_us();
+      if (now < next_tick)
+	wait_us(next_tick - now);
+      next_tick += 1000;
+      myled = 0;
+      oe = 1;
       lat = 1;
       select_row(row);
       lat = 0;
@@ -79,10 +217,7 @@ int main()
       row++;
       if (row == 8) {
 	  row = 0;
-	  delay++;
 	  x++;
       }
-      wait_ms(1);
-      oe = 1;
   }
 }
