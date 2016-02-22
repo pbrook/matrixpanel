a14a409e9d589aa440e3fc0637412de30a703449
Paul Brook <paul@nowt.org>
2016-02-22 23:28:16 +0000

    Split matrix driver into separate file
    Signed-off-by: Paul Brook <paul@nowt.org>

--- client/sender.py
+++ client/sender.py
@@ -1,43 +1,9 @@
 #! /usr/bin/env python3
 
-import socket
 import time
 import math
 import numpy as np
-
-TARGET_IP = "192.168.11.2"
-TARGET_PORT = 3001
-
-class MatrixScreen():
-    X = 64
-    Y = 64
-
-    ROWS = 8
-    COLUMNS = (X * Y // (ROWS * 2))
-    FRAGMENT_SIZE = 512
-    FRAGMENTS_PER_ROW = (X * Y * 4) // (FRAGMENT_SIZE * ROWS)
-
-    def __init__(self):
-        self.screen = np.zeros((self.X, self.Y), dtype = np.uint32)
-        self._socket = socket.socket(type=socket.SOCK_DGRAM)
-        self._frame_num = 0
-
-    def send(self):
-        dest = (TARGET_IP, TARGET_PORT)
-        n = 0
-        for y0 in range(0, self.ROWS):
-            for y1 in range(self.FRAGMENTS_PER_ROW):
-                y = y0 + y1 * self.ROWS * 2
-                header = bytes([3, self._frame_num, n, 0])
-                buf = header + bytes(self.screen[:, y]) + bytes(self.screen[:, y + self.ROWS])
-                self._socket.sendto(buf, dest);
-                self._socket.recv(8)
-                n += 1
-        self._socket.sendto(bytes([1, self._frame_num, 0, 0]), dest)
-        self._socket.recv(8)
-        self._frame_num += 1
-        if self._frame_num == 256:
-            self._frame_num = 0
+from matrixscreen import MatrixScreen
 
 def pos_modf(val):
     val = math.modf(val)[0]
@@ -67,7 +33,7 @@ def color_plasma(val):
 offset = 0.0
 DT = 1/30
 
-def tick(screen):
+def draw(screen):
     global offset
     sz = 64
     scale = 1.5 * math.pi * 2.0 / float(sz)
@@ -92,7 +58,7 @@ def main():
                 frames = 0
                 next_tick += 1.0
             frames += 1
-            tick(m.screen)
+            draw(m.screen)
             m.send()
     except:
         m.screen[:] = 0
